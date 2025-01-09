c4fe6e608f306ce513e9b7a2547fd7820fa86aee
Rudra Gadekar <140463533+RudraGB@users.noreply.github.com>
2025-01-09 22:50:08 +0000

    python programs

--- char_occur.py
+++ char_occur.py
@@ -7,3 +7,15 @@ for char in unique_char:
     occurence = sentence.count(char)
     print(f"{char} occurs :{occurence} times")
 
+#Input: 
+#string = "engineers rock"
+#pattern = "er";
+
+
+def check_pattern(input, pattern):
+    if pattern in input:
+        print(f"Pattern {pattern} found in {input}")
+    else:
+        print(f"{pattern} not found in input {input}")
+
+check_pattern("engineers rock","er")
